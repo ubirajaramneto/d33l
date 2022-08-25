37253fbcc41f8bac2a78fa18b55cf7c239b20066
Ubirajara Marsicano Neto <ubirajara_neto@hotmail.com>
2022-08-25 11:14:41 -0300

    feat(auto-complete): implement basic component, implement change event loop, implement debouncing

--- apps/web-app/src/app/app.tsx
+++ apps/web-app/src/app/app.tsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
+import {AutoComplete} from "@d33l/ui";
 
 export function App() {
   return (
-    <>
-      <div />
-    </>
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+      <AutoComplete/>
+    </div>
   );
 }
 
--- libs/ui/src/lib/auto-complete/auto-complete.tsx
+++ libs/ui/src/lib/auto-complete/auto-complete.tsx
@@ -1,12 +1,36 @@
 import styles from './auto-complete.module.scss';
+import React, { useEffect, useState } from 'react';
 
 /* eslint-disable-next-line */
 export interface AutoCompleteProps {}
 
 export function AutoComplete(props: AutoCompleteProps) {
+  const [searchTerm, setSearchTerm] = useState('');
+  let timeout: NodeJS.Timeout;
+
+  // we don't want to search on every keystroke, so we will debounce here.
+  const handleSearchTermChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (timeout) clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      setSearchTerm(e.target.value);
+    }, 500);
+  };
+
+  useEffect(() => {
+    console.log('Im changing');
+  }, [searchTerm]);
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to AutoComplete!</h1>
+    <div>
+      <input
+        id="auto-complete-input"
+        type="text"
+        placeholder="Search for a country"
+        className="p-8 border-solid border-2 border-slate-300 rounded-lg text-2xl text-slate-500"
+        onChange={handleSearchTermChange}
+      />
+      <div id="auto-complete-container"></div>
     </div>
   );
 }
