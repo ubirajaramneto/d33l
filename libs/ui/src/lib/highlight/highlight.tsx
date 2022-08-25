4f1ade6c65788887abb130b238f94dce1f8086b8
Ubirajara Marsicano Neto <ubirajara_neto@hotmail.com>
2022-08-25 13:05:24 -0300

    chore(highlight): explain algorithm

--- libs/ui/src/lib/highlight/highlight.tsx
+++ libs/ui/src/lib/highlight/highlight.tsx
@@ -11,16 +11,20 @@ export function Highlight({ term, text }: HighlightProps) {
     return <span>{text}</span>;
   }
   const regex = new RegExp(`(${term})`, 'gi');
+  // First we need to split the string based on the search term
   const sections = text.split(regex);
+
+  // After we have all the sections of the string in an array,
+  // if the section is equal to the term, wrap it with the mark tag, if not, return a normal span tag
   return (
     <span>
       {sections
-        .filter((part) => part)
-        .map((part, i) =>
-          regex.test(part) ? (
-            <mark key={i}>{part}</mark>
+        .filter((section) => section)
+        .map((section, i) =>
+          regex.test(section) ? (
+            <mark key={i}>{section}</mark>
           ) : (
-            <span key={i}>{part}</span>
+            <span key={i}>{section}</span>
           )
         )}
     </span>
